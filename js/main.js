f9a275f032a633ca39b0642fbe1e87f28bce6d7b
Hannah Helms <helmshannahpatrice@gmail.com>
2017-12-05 00:32:45 -0500

    Insert hover effect on home nav using JS

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
 
     var gallery = $('.gallery a').simpleLightbox();
 
+    $('.homenav').css({'opacity': 0.6});
+    $('.homenav').mouseenter(function () {
+        $(this).css({'opacity': 1});
+    });
+    $('.homenav').mouseleave(function () {
+        $(this).css({'opacity': 0.6});
+    });
+
 });
