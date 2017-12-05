b77b97bee734f09cde903e6ed0a23656feac307c
Hannah Helms <helmshannahpatrice@gmail.com>
2017-12-05 00:34:18 -0500

    Update JS home nav effect for mobile
    Remove JS styling and change to CSS for screens over 1200, keep
    hover effect in JS.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
 
     var gallery = $('.gallery a').simpleLightbox();
 
-    $('.homenav').css({'opacity': 0.6});
     $('.homenav').mouseenter(function () {
         $(this).css({'opacity': 1});
     });
