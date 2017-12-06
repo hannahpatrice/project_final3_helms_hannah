25151b6752348da43ca89f49c489c3c0e72d453e
Hannah Helms <helmshannahpatrice@gmail.com>
2017-12-05 23:46:37 -0500

    Change about image on click
    Also attempt js calendar, and other minor edits.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 ##RESOURCES
 -[Textillate PlugIn to animate homepage h1 text](https://github.com/jschr/textillate)
 -[Lightbox PlugIn for Gallery](https://www.jqueryscript.net/lightbox/Responsive-Touch-enabled-jQuery-Image-Lightbox-Plugin.html)
+-[Calendar PlugIn for Races Page](https://www.jqueryscript.net/time-clock/Creating-A-Pretty-Event-Calendar-with-jQuery.html)
 
 ##DEDUCTIONS
 I reviewed the list of deductions for this project and in the syllabus.
--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
 
+// LIGHTBOX
+
     var gallery = $('.gallery a').simpleLightbox();
+
+// HOME PAGE MENU HOVER
 
     $('.homenav').mouseenter(function () {
         $(this).css({'opacity': 1});
@@ -9,8 +13,23 @@ $(document).ready(function () {
         $(this).css({'opacity': 0.6});
     });
 
+//HOME PAGE TEXT EFFECTS
+
     $(function () {
         $('.lettering').textillate({ in: { effect: 'fadeInRight' } });
     });
 
+//CUSTOM JS
+
+    var changeImg = document.getElementById('changeme');
+
+    changeImg.addEventListener('click', function () {
+        if (document.getElementById('changeme').src = 'img/aboutside.jpg') {
+            document.getElementById('changeme').src = 'img/aboutside2.jpg'
+        }
+		// WHY IS THIS BELOW NOT WORKING
+		//else {
+        //    document.getElementById('changeme').src = 'img/aboutside.jpg';
+        //}
+    });
 });
